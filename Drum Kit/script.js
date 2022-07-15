52a0946045a57bd7d12f934bd904683eb292e216
ntd8466 <ntd8466@gmail.com>
2022-07-15 15:44:13 +0700

    add script

---
+++ Drum Kit/script.js
@@ -0,0 +1,13 @@
+window.addEventListener('keydown',function(e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    key.classList.add('playing');
+    audio.play();
+})
+
+window.addEventListener('keyup',function(e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    key.classList.remove('playing');
+    audio.currentTime = 0;
+})
